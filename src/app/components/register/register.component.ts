c46c44845fca31597c8d1384f971f97c304394e5
Guan He <guan.he@slalom.com>
2020-04-13 17:59:28 -0400

    added form validation to registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
@@ -13,17 +13,35 @@ export class RegisterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
 
   ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      username: "",
-      email: "",
-      confirmEmail: "",
-      password: "",
-      confirmPassword: ""
-    });
+    this.signupForm = this.formBuilder.group(
+      {
+        username: ["", [Validators.required, Validators.maxLength(64)]],
+        email: ["", [Validators.required, Validators.email]],
+        confirmEmail: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ["", [Validators.required, Validators.minLength(8)]]
+      },
+      { validators: [this.emailMatchValidator, this.passwordMatchValidator] }
+    );
+  }
+
+  emailMatchValidator(signupForm: FormGroup) {
+    return signupForm.controls["email"].value ===
+      signupForm.controls["confirmEmail"].value
+      ? null
+      : { mismatch: true };
+  }
+
+  passwordMatchValidator(signupForm: FormGroup) {
+    return signupForm.controls["password"].value ===
+      signupForm.controls["confirmPassword"].value
+      ? null
+      : { mismatch: true };
   }
 
   onSubmit(signupForm: FormGroup): void {
     console.log(signupForm.value);
+    console.log(signupForm);
     const {
       username,
       email,
